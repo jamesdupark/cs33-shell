7fd8c8514f6aab386a266cfc901b6e7e633d1ef6
jamesdupark <james_m_park@brown.edu>
2021-11-04 17:36:34 -0400

    sighandler_t is a struct

--- sh.c
+++ sh.c
@@ -25,7 +25,7 @@
  * and SIGTTOU to default
  *
  */
-void change_def_handlers(void handler) {
+void change_def_handlers(void (*handler)) {
     checked_signal(SIGINT, handler);
     checked_signal(SIGTSTP, handler);
     checked_signal(SIGTTTOU, handler);
